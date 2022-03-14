f219ad72837be671f600ed018190faccdae1ed44
Luisma <luismadf95@gmail.com>
2022-03-14 17:03:41 -0400

    first edit of the project, some interface done

---
+++ src/components/todoList.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        Todo List of Luis Manuel
+        <input type="text" class="todo-input" placeholder="Whats need to be done" v-model="newTodo">
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'todo-List',
+    data() {
+        return {
+            newTodo: '',
+            todos: [
+                {
+                    'id': 1,
+                    'title': 'Buy eggs',
+                    'completed': false
+                    
+                }, {
+                    'id': 2,
+                    'title': 'Buy milk',
+                    'completed': false 
+                }, {
+                    'id': 3,
+                    'title': 'Buy Chocolate',
+                    'completed': false
+                }
+            ]
+        }
+    }
+}
+
+</script>
+
+<style lang="scss">
+
+.todo-input {
+    width: 100%;
+    padding: 10px 18px;
+    font-size: 18px;
+    margin-button: 16px;
+
+    &:focus{
+        outline: 0;
+    }
+}
+
+</style>
